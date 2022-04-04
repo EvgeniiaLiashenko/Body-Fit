0b1552bd528147e5bc6709f4fe6721668314292b
EvgeniiaLiashenko <jennietols@gmail.com>
2022-04-04 20:48:15 +0300

    first commit

---
+++ Верска сайта Катя/js/main.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+
+
+
+    $('.map__iframe').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+
+        fixedContentPos: false
+    });
+
+    $('.gallery__items').slick({
+        lazyLoad: 'ondemand',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true
+
+
+    });
+
+
+
+    $('.gallery__items').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: function(item) {
+                return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+            }
+        }
+    });
+
+    $('.trainings__link').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+
+        fixedContentPos: false
+    });
+
+    $('.header__burger').click(function(event) {
+        $('.header__burger, .menu').toggleClass('active');
+        $('.body').toggleClass('lock');
+    });
+
+});
